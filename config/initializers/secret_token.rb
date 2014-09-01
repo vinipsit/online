4cb5eebe19159b7b64d9af78040df5b00019ccc0
vineeta yadav <vineeta.vini1@gmail.com>
2014-09-01 03:39:10 -0700

    1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shopping::Application.config.secret_key_base = 'a756da6081365cf086ad93e0c719b7f514128ce7457ca08159110a69a2ddcf051c93926d120e31ead3263e8211a702fcda7b6adcc380a88ed31341a3d90d6f72'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shopping::Application.config.session_store :cookie_store, key: '_shopping_session'
